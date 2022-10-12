31e16d0362abcd793d9f49d4ab74b61346b87e63
kaleg <92884839+larsbritz@users.noreply.github.com>
2022-10-12 22:51:16 +0200

    cleaned up some code for rngsub

--- src/Commands/Rngsub.ts
+++ src/Commands/Rngsub.ts
@@ -1,9 +1,7 @@
 import DiscordJS, { ApplicationCommandOptionType } from "discord.js";
 import { NormalCommandClass } from "../utils/Commands/NormalCommand/NormalCommand";
+import { getPostUrl } from "../utils/reddit/getRandomPost";
 const snoowrap = require("snoowrap");
-
-let reddit_api: any = undefined;
-
 class RngSub extends NormalCommandClass {
   name: string = "rngsub";
   description: string = "gives a random post from a subreddit";
@@ -19,9 +17,9 @@ class RngSub extends NormalCommandClass {
 
   async reply(interaction: DiscordJS.CommandInteraction) {
     await interaction.reply({
-      content: "Hold on REDDIT's is dogshit",
+      content: "Hold on my hosts' internet is dogshit",
     });
-    const content = await getSubreddit(interaction);
+    const content = await getPostUrl(interaction);
     await interaction.editReply({ content: content }).catch((err: Error) => {
       console.log(err);
     });
@@ -30,40 +28,4 @@ class RngSub extends NormalCommandClass {
 
 export function getInstance() {
   return new RngSub();
-}
-
-async function getSubreddit(
-  interaction: DiscordJS.CommandInteraction
-): Promise<string> {
-  let post = await getPost(parseOption(interaction));
-  return !post.permalink
-    ? "Reddit returned undefined (Subreddit disabled random)"
-    : "http://reddit.com" + post.permalink;
-}
-
-function createAPI() {
-  reddit_api = new snoowrap({
-    userAgent: process.env.REDDIT_USERAGENT,
-    clientId: process.env.REDDIT_CLIENTID,
-    clientSecret: process.env.REDDIT_CLIENTSECRET,
-    username: process.env.REDDIT_USERNAME,
-    password: process.env.REDDIT_PW,
-  });
-}
-
-function parseOption(interaction: DiscordJS.CommandInteraction): string {
-  if (!interaction.options.get("optional_subreddit")) return "Random";
-  const parse = String(interaction.options.get("optional_subreddit")?.value);
-  return parse;
-}
-
-async function getPost(reddit: string): Promise<any> {
-  createAPI();
-  try {
-    let subreddit = await reddit_api.getSubreddit(reddit);
-    let randomSub = await subreddit.getRandomSubmission();
-    return randomSub;
-  } catch (err) {
-    return "Command failed";
-  }
 }
