abcedd0f6c365c0d99a35072b24520508a5b9dd6
kaleg <92884839+larsbritz@users.noreply.github.com>
2022-10-14 20:57:49 +0200

    added some exception handling

--- src/utils/reddit/getRandomPost.ts
+++ src/utils/reddit/getRandomPost.ts
@@ -1,23 +1,31 @@
 const snoowrap = require("snoowrap");
-import DiscordJS, { ApplicationCommandOptionType } from "discord.js";
+import DiscordJS, {
+  ApplicationCommandOptionType,
+  SlashCommandSubcommandGroupBuilder,
+} from "discord.js";
 
 export async function getPostUrl(
   interaction: DiscordJS.CommandInteraction
 ): Promise<string> {
   let post = await getPost(parseOption(interaction));
   return !post.permalink
-    ? "Reddit returned undefined (Subreddit disabled random)"
+    ? "Reddit returned undefined"
     : "http://reddit.com" + post.permalink;
 }
 
 function createAPI() {
-  return new snoowrap({
-    userAgent: process.env.REDDIT_USERAGENT,
-    clientId: process.env.REDDIT_CLIENTID,
-    clientSecret: process.env.REDDIT_CLIENTSECRET,
-    username: process.env.REDDIT_USERNAME,
-    password: process.env.REDDIT_PW,
-  });
+  try {
+    return new snoowrap({
+      userAgent: process.env.REDDIT_USERAGENT,
+      clientId: process.env.REDDIT_CLIENTID,
+      clientSecret: process.env.REDDIT_CLIENTSECRET,
+      username: process.env.REDDIT_USERNAME,
+      password: process.env.REDDIT_PW,
+    });
+  } catch (err) {
+    console.log("invalid or missing credentials (redditAPI)");
+    return undefined;
+  }
 }
 
 function parseOption(interaction: DiscordJS.CommandInteraction): string {
@@ -33,6 +41,7 @@ async function getPost(reddit: string): Promise<any> {
     let randomSub = await subreddit.getRandomSubmission();
     return randomSub;
   } catch (err) {
-    return "Command failed";
+    console.log("command failed(getRandomPost)");
+    return " ";
   }
 }
