de470eaf27f716416046416abfb7795ef764c3f2
doniel-t <daniel.theil.g@gmail.com>
2023-09-07 20:30:10 +0200

    fixed ? get random move

--- src/utils/Movie/randMovie.ts
+++ src/utils/Movie/randMovie.ts
@@ -1,7 +1,7 @@
 import { Movie } from "./fetchMovieData";
 
 export function getRandomMovie(movieList: Movie[]): Movie {
-  let randIndex = Math.floor(Math.random() * movieList.length - 1);
+  let randIndex = Math.floor(Math.random() * movieList.length);
   let randMovie = movieList[randIndex];
   /*while (!replyStr.includes(randMovie.name)) {
     randIndex = Math.floor(Math.random() * movieList.length);
