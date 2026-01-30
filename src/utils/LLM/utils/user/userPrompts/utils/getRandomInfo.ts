fd898e91df4a4f3845fb91a6ac264d0f03668809
daniel.theil@komma-d.de <daniel.theil@komma-d.de>
2026-01-30 21:05:46 +0100

    adjusted: system prompt for user info

--- src/utils/LLM/utils/user/userPrompts/utils/getRandomInfo.ts
+++ src/utils/LLM/utils/user/userPrompts/utils/getRandomInfo.ts
@@ -1,3 +1,5 @@
+const AMOUNT_INFO_USED = 0.1;
+
 export function getRandomInfo(info: string[], amount?: number) {
-    return info.sort(() => Math.random() - 0.5).slice(0, amount ?? Math.floor(info.length * 0.33))
+    return info.sort(() => Math.random() - 0.5).slice(0, amount ?? Math.floor(info.length * AMOUNT_INFO_USED))
 }
--- src/utils/LLM/utils/user/userPrompts/utils/getUserPrompt.ts
+++ src/utils/LLM/utils/user/userPrompts/utils/getUserPrompt.ts
@@ -2,7 +2,9 @@ import { User } from "../../../../types";
 import { getRandomInfo } from "./getRandomInfo";
 
 export function getUserPrompt(user: User) {
-    const userPrompt = `${user.prompt}\n\n# Information about the user\n${getRandomInfo(user.userInformation).join("\n")}`;
+    const info = getRandomInfo(user.userInformation).join("\n");
 
-    return userPrompt;
+    return `${user.prompt}\n\n# Background context about the user\n
+        The following is supplementary background info. Do NOT structure your responses around this info or reference it directly unless it's clearly relevant to the conversation. Focus on responding naturally to the message history.
+        \n\n${info}`;
 }
